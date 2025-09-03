834d589002845165fac7bad4ef22810c42e7ae17
manish_patidar <shanimpatidar@gmail.com>
2025-09-03 17:31:03 +0530

    comppleted with room join for peers

--- server.js
+++ server.js
@@ -66,18 +66,70 @@ io.on('connection', (socket) => {
 
                 clients.forEach(clientId=>{
                         // event emit
-                        //p2p connection
+                        //p2p connection - client ko bol rhee hai hum 
                         io.to(clientId).emit(ACTIONS.ADD_PEER,{
-
+                                peerId:socket.id,
+                                createOffer:false,//i will do that
+                                user
                         })
-                })
 
-                socket.emit(ACTIONS.ADD_PEER,{})
+                         socket.emit(ACTIONS.ADD_PEER,{
+                                peerId:clientId,
+                                createOffer:true,
+                                user:socketUserMapping[clientId]
+                        })
+
+                })
 
                 socket.join(roomId)
 
-                
         })
+
+        // handle relay ice
+        socket.on(ACTIONS.RELAY_ICE, ({peerId,icecandidate})=>{
+                io.to(peerId).emit(ACTIONS.ICE_CANDIDATE,{
+                        peerId:socket.id,
+                        icecandidate
+                })
+        })
+
+        // handle relay sdp
+        socket.on(ACTIONS.RELAY_SDP, ({peerId,sessionDescription})=>{
+                io.to(peerId).emit(ACTIONS.SESSION_DESCRIPTION,{
+                        peerId:socket.id,
+                        sessionDescription
+                })
+        })
+
+        // handle remove peer
+        // handle leave the room
+
+        const leaveRoom = ({roomId}) =>{
+                const {rooms} = socket
+
+                Array.from(rooms).forEach(roomId=>{
+                        const clients = Array.from(io.sockets.adapter.rooms.get(roomId) || [])
+
+                        clients.forEach(clientId=>{
+                                io.to(clientId).emit(ACTIONS.REMOVE_PEER,{
+                                        peerId:socket.id,
+                                        userId:socketUserMapping[socket.id]._id
+                                })
+
+                                socket.emit(ACTIONS.REMOVE_PEER,{
+                                        peerId:clientId,
+                                        userId:socketUserMapping[clientId]._id
+                                })
+                        })
+
+                        socket.leave(roomId)
+                })
+
+               delete socketUserMapping[socket.id]
+        }
+
+        socket.on(ACTIONS.LEAVE,leaveRoom)
+
 })
 
 
